180c6c191721bf31bc62038f1f9662b11b9a8ecf
Lucas Carioca <lucascarioca97@gmail.com>
2022-08-08 16:56:23 -0300

    implemented NoteViewModel

--- app/src/main/java/com/example/jetnote/MainActivity.kt
+++ app/src/main/java/com/example/jetnote/MainActivity.kt
@@ -3,18 +3,19 @@ package com.example.jetnote
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.jetnote.data.NotesDataSource
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.jetnote.model.Note
 import com.example.jetnote.screens.NoteScreen
+import com.example.jetnote.screens.NoteViewModel
 import com.example.jetnote.ui.theme.JetNoteTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,21 +28,27 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    val notes = remember {
-                        mutableStateListOf<Note>()
-                    }
+                    val noteViewModel: NoteViewModel by viewModels()
+                    NotesApp(noteViewModel)
 
-                    NoteScreen(notes = notes,
-                        onAddNote = {
-                            notes.add(it)
-                        },
-                        onRemoveNote = {
-                            notes.remove(it)
-                        })
                 }
             }
         }
     }
+}
+
+@Composable
+fun NotesApp(noteViewModel: NoteViewModel = viewModel()) {
+
+    val notesList = noteViewModel.getAllNotes()
+
+    NoteScreen(notes = notesList,
+        onAddNote = {
+            noteViewModel.addNote(it)
+        },
+        onRemoveNote = {
+            noteViewModel.removeNote(it)
+        })
 }
 
 
